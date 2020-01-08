48de45e2ff7d2127c0fb0643d4123c3df2e47b42
Hendi Santika <hendisantika@gmail.com>
2020-01-09 06:55:53 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootcrudmysqlvuejs/SpringbootCrudMysqlVuejsApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcrudmysqlvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudMysqlVuejsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCrudMysqlVuejsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootcrudmysqlvuejs/SpringbootCrudMysqlVuejsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcrudmysqlvuejs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudMysqlVuejsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
