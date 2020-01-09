3ea9ebef97de32a327aa77f8a38e9722f7d661d9
Hendi Santika <hendisantika@gmail.com>
2020-01-09 07:02:19 +0700

    #7 - Add ProductService

---
+++ src/main/java/com/hendisantika/springbootcrudmysqlvuejs/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.hendisantika.springbootcrudmysqlvuejs.service;
+
+import com.hendisantika.springbootcrudmysqlvuejs.entity.Product;
+import com.hendisantika.springbootcrudmysqlvuejs.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-mysql-vuejs
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/01/20
+ * Time: 07.01
+ */
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product stock) {
+        return productRepository.save(stock);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
