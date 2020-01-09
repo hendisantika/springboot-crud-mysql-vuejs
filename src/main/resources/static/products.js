fa97a67c1813a86cc28cf4fb0bd889c25182a96f
Hendi Santika <hendisantika@gmail.com>
2020-01-09 07:07:15 +0700

    #18 - Add productService function in products.js file

--- src/main/resources/static/products.js
+++ src/main/resources/static/products.js
@@ -12,3 +12,40 @@ function findProductKey(productId) {
         }
     }
 }
+
+var productService = {
+    findAll(fn) {
+        axios
+            .get('/api/v1/products')
+            .then(response => fn(response))
+            .catch(error => console.log(error))
+    },
+
+    findById(id, fn) {
+        axios
+            .get('/api/v1/products/' + id)
+            .then(response => fn(response))
+            .catch(error => console.log(error))
+    },
+
+    create(product, fn) {
+        axios
+            .post('/api/v1/products', product)
+            .then(response => fn(response))
+            .catch(error => console.log(error))
+    },
+
+    update(id, product, fn) {
+        axios
+            .put('/api/v1/products/' + id, product)
+            .then(response => fn(response))
+            .catch(error => console.log(error))
+    },
+
+    deleteProduct(id, fn) {
+        axios
+            .delete('/api/v1/products/' + id)
+            .then(response => fn(response))
+            .catch(error => console.log(error))
+    }
+};
