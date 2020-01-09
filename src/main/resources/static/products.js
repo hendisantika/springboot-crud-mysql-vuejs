7be3f45f6e9374524f3b30b1453f961c6b3b269a
Hendi Santika <hendisantika@gmail.com>
2020-01-09 07:06:49 +0700

    #17 - Add findProductKey function in products.js file

--- src/main/resources/static/products.js
+++ src/main/resources/static/products.js
@@ -3,3 +3,12 @@ var products = [];
 function findProduct(productId) {
     return products[findProductKey(productId)];
 }
+
+
+function findProductKey(productId) {
+    for (var key = 0; key < products.length; key++) {
+        if (products[key].id == productId) {
+            return key;
+        }
+    }
+}
