69db17da1c10be28894e6c55d9a4f945cf0c8000
Hendi Santika <hendisantika@gmail.com>
2020-01-09 07:04:05 +0700

    #12 - Add update API  in ProductAPI

--- src/main/java/com/hendisantika/springbootcrudmysqlvuejs/controller/ProductAPI.java
+++ src/main/java/com/hendisantika/springbootcrudmysqlvuejs/controller/ProductAPI.java
@@ -48,4 +48,14 @@ public class ProductAPI {
         return ResponseEntity.ok(stock.get());
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @Valid @RequestBody Product product) {
+        if (!productService.findById(id).isPresent()) {
+            log.error("Id " + id + " is not existed");
+            ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(productService.save(product));
+    }
+
 }
