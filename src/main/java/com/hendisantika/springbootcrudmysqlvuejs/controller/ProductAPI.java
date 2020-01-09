1eaf0c97f9a8b4b81c4af131317d3b41634a17f8
Hendi Santika <hendisantika@gmail.com>
2020-01-09 07:03:21 +0700

    #9 - Add findAll API  in ProductAPI

--- src/main/java/com/hendisantika/springbootcrudmysqlvuejs/controller/ProductAPI.java
+++ src/main/java/com/hendisantika/springbootcrudmysqlvuejs/controller/ProductAPI.java
@@ -1,10 +1,15 @@
 package com.hendisantika.springbootcrudmysqlvuejs.controller;
 
+import com.hendisantika.springbootcrudmysqlvuejs.entity.Product;
 import com.hendisantika.springbootcrudmysqlvuejs.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProductAPI {
     private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        return ResponseEntity.ok(productService.findAll());
+    }
 }
